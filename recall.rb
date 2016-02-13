26cb9949d0a35e3b7f04d67c487fce6610eb9f40
Milan Grubnić <MilanG@aol.com>
2016-02-13 16:16:05 -0600

    debugged require to data_mapper

--- recall.rb
+++ recall.rb
@@ -1,6 +1,6 @@
 require "rubygems"
 require "sinatra"
-require "datamapper"
+require "data_mapper"
 
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/recall.db") # set up database in currrent directory
 
