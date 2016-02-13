ac8ea57cfd66a27894aa0b514456abfeeba6fccb
Milan Grubnić <MilanG@aol.com>
2016-02-13 15:55:45 -0600

    added comments

--- recall.rb
+++ recall.rb
@@ -2,15 +2,15 @@ require "rubygems"
 require "sinatra"
 require "datamapper"
 
-DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/recall.db")
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/recall.db") # set up database in currrent directory
 
-class Note
+class Note # create table
 	include DataMapper::Resource
-	property :id, Serial
+	property :id, Serial # auto-incrementing key
 	property :content, Text, :required => true
 	property :complete, Boolean, :required => true, :default => false
 	property :created_at, DateTime
 	property :updated_at, DateTime
 end
 
-DataMapper.finalize.auto_upgrade!
+DataMapper.finalize.auto_upgrade! #automatically update database after changes
